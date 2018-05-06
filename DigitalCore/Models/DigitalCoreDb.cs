b565ac056484cccadf37c320cf293d0c9a1fdb55
lipesco <filipe.branco@gmail.com>
2018-05-06 02:42:24 +0100

    adicionado a Migrations

---
+++ DigitalCore/Models/DigitalCoreDb.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace DigitalCore.Models
+{
+    public class DigitalCoreDb : DbContext
+    {
+
+        // criar o construtor
+        public DigitalCoreDb() : base("DigitalCoreDbConnectionString")
+        {
+
+        }
+
+        // definir as 'tabelas' da base de dados
+        public virtual DbSet<Cliente> Cliente { get; set; }
+        public virtual DbSet<Compra> Compra { get; set; }
+        public virtual DbSet<TipoPagamento> TipoPagamento { get; set; }
+        public virtual DbSet<ProdutoCompra> ProdutoCompra { get; set; }
+        public virtual DbSet<Produto> Produto { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+            modelBuilder.Conventions.Remove<ManyToManyCascadeDeleteConvention>();
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
